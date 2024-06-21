5c258e267418b048bf9ab8fab787878e85bda6c0
Devops0895 <103020833+Devops0895@users.noreply.github.com>
2024-06-21 20:34:21 +0530

    Add files via upload
    files

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region  = "us-west-2"
+  profile = "Administrator"
+}
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = var.s3_bucket_name
+# }
---
+++ s3-lambda-role.tf
@@ -0,0 +1,49 @@
+# Create IAM role for EC2 instances
+resource "aws_iam_role" "s3_lambda_access_role" {
+  name               = "EC2S3AccessRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+# Update the IAM policy for S3 access
+resource "aws_iam_policy" "s3_lambda_access_policy" {
+  name        = "S3AccessPolicy"
+  description = "Policy for EC2 S3 access"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+        "s3:PutObject"
+      ],
+      "Resource": "arn:aws:s3:::*"
+    }
+  ]
+}
+EOF
+}
+
+
+# Attach the updated IAM policy to the IAM role
+resource "aws_iam_role_policy_attachment" "s3_access_policy_attachment_role" {
+  policy_arn = aws_iam_policy.s3_lambda_access_policy.arn
+  role       = aws_iam_role.s3_lambda_access_role.name
+}
